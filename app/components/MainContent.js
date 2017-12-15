0f7f3ae980a360fe096e81987acf320657e30fb1
Catalin Hoha <c.hoha@lukkien.com>
2017-12-15 13:57:50 +0200

    Added project files

---
+++ app/components/MainContent.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+				<h1> Welcome! </h1>
+				<h2> React-Babel-Webpack is working ! </h2>
+            </div>
+        );
+    }
+}
---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import MainContent from "./components/MainContent";
+
+ReactDOM.render(
+	<MainContent />
+    , document.getElementById("root"));
